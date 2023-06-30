3dcaa55c58d8bd86b010258b9f2c060053a2af60
nastykovalchuk <kovalchuk.anastasiya.m@yandex.ru>
2023-06-30 20:45:38 +0300

    fix eslint error

--- backend/middlewares/logger.js
+++ backend/middlewares/logger.js
@@ -1,5 +1,5 @@
 const winston = require('winston');
-const expressWinston = require('express-winston')
+const expressWinston = require('express-winston');
 
 const requestLogger = expressWinston.logger({
   transports: [new winston.transports.File({ filename: 'request.log' })],
