b9a92e9e22a533aaf549799ebfcf313b034e1dbb
nastykovalchuk <kovalchuk.anastasiya.m@yandex.ru>
2023-06-30 20:47:54 +0300

    fix port for tests

--- backend/configs/main.js
+++ backend/configs/main.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 const JWT_SECRET = process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'dev-secret';
 
-const { PORT = 2000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 module.exports = { JWT_SECRET, PORT, DB_URL };
